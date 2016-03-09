e6e535de0a778e6c75b847bf80f274848e6a5823
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-03-09 17:30:09 +1100

    Reorder database configuration for production.

--- demo/settings/production.py
+++ demo/settings/production.py
@@ -15,18 +15,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
-if os.environ.get('OPENSHIFT_DATABASE_TYPE') == 'PostgreSQL':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('OPENSHIFT_POSTGRESQL_DB_NAME'),
-            'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
-            'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
-            'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
-            'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT')
-        }
-    }
-elif os.environ.get('OPENSHIFT_DATABASE_TYPE') == 'MySQL':
+if os.environ.get('OPENSHIFT_DATABASE_TYPE') == 'MySQL':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
@@ -35,5 +24,16 @@ elif os.environ.get('OPENSHIFT_DATABASE_TYPE') == 'MySQL':
             'PASSWORD': os.environ.get('OPENSHIFT_MYSQL_DB_PASSWORD'),
             'HOST': os.environ.get('OPENSHIFT_MYSQL_DB_HOST'),
             'PORT': os.environ.get('OPENSHIFT_MYSQL_DB_PORT')
+        }
+    }
+elif os.environ.get('OPENSHIFT_DATABASE_TYPE') == 'PostgreSQL':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('OPENSHIFT_POSTGRESQL_DB_NAME'),
+            'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
+            'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
+            'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
+            'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT')
         }
     }
