608fb61e115ddce6b099d27be379df85338b7410
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-03-09 15:31:31 +1100

    Update production to use environment variables set by OpenShift configuration generator.

--- demo/settings/production.py
+++ demo/settings/production.py
@@ -15,13 +15,25 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRESQL_DATABASE', 'wagtail'),
-        'USER': os.environ.get('POSTGRESQL_USER', 'wagtail'),
-        'PASSWORD': os.environ.get('POSTGRESQL_PASSWORD', 'wagtail'),
-        'HOST': os.environ.get('POSTGRESQL_HOST', 'wagtaildb'),
-        'PORT': os.environ.get('POSTGRESQL_PORT', '5432'),
+if os.environ.get('OPENSHIFT_DATABASE_TYPE') == 'PostgreSQL':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('OPENSHIFT_POSTGRESQL_DB_NAME'),
+            'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
+            'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
+            'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
+            'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT')
+        }
     }
-}
+elif os.environ.get('OPENSHIFT_DATABASE_TYPE') == 'MySQL':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('OPENSHIFT_MYSQL_DB_NAME'),
+            'USER': os.environ.get('OPENSHIFT_MYSQL_DB_USERNAME'),
+            'PASSWORD': os.environ.get('OPENSHIFT_MYSQL_DB_PASSWORD'),
+            'HOST': os.environ.get('OPENSHIFT_MYSQL_DB_HOST'),
+            'PORT': os.environ.get('OPENSHIFT_MYSQL_DB_PORT')
+        }
+    }
